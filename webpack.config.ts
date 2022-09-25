11e02683f9ebc29eeb5ca0ab2eb960fd89bb7e15
Fawad Ali <m.fawaadali98@gmail.com>
2022-09-25 21:06:39 +0500

    Basic webpack setup

---
+++ webpack.config.ts
@@ -0,0 +1,47 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
+const isDevServer = process.env.WEBPACK_SERVE ?? false;
+
+const config: Configuration = {
+  mode: isDevServer ? 'development' : 'production',
+  context: path.resolve(__dirname, './'),
+  entry: path.resolve(__dirname, './src/index.ts'),
+  devtool: isDevServer ? 'inline-source-map' : false,
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'esbuild-loader',
+        options: {
+          loader: 'ts',
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif|gltf|obj|fbx)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[folder]/[name].[ext]',
+          outputPath: 'assets',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+};
+
+export default config;
